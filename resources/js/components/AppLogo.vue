f754eaae75d0706af6165ba0cb3b021b1d046deb
combizera <ycombi349@gmail.com>
2026-01-21 18:17:52 -0300

    feat: initial commit

---
+++ resources/js/components/AppLogo.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import AppLogoIcon from '@/components/AppLogoIcon.vue';
+</script>
+
+<template>
+    <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
+    <div class="grid flex-1 text-left">
+        <span class="truncate leading-tight font-semibold">
+            Prevê
+        </span>
+    </div>
+</template>
