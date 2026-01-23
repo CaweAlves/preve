96b97dd4f325e8d313787cdf6e0a4e1dda44f5a5
combizera <ycombi349@gmail.com>
2026-01-23 00:24:27 -0300

    chore: install tables ui

---
+++ resources/js/pages/Category.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+
+import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
+import AppLayout from '@/layouts/AppLayout.vue';
+import categories from '@/routes/categories';
+import { type BreadcrumbItem } from '@/types';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Category',
+        href: categories.index().url,
+    },
+];
+</script>
+
+<template>
+    <Head title="Category" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div
+            class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4"
+        >
+            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div
+                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
+                >
+                    oi
+                    <PlaceholderPattern />
+                </div>
+                <div
+                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
+                >
+                    <PlaceholderPattern />
+                </div>
+                <div
+                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
+                >
+                    <PlaceholderPattern />
+                </div>
+            </div>
+            <div
+                class="relative min-h-full flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border"
+            >
+                <PlaceholderPattern />
+            </div>
+        </div>
+    </AppLayout>
+</template>
