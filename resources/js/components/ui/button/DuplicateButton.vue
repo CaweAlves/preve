f4b380dcf7f76b06df16aed36e6632cb94c7cffb
combizera <ycombi349@gmail.com>
2026-01-27 18:29:15 -0300

    feat: add DuplicateButton component

---
+++ resources/js/components/ui/button/DuplicateButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Copy } from 'lucide-vue-next';
+
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
+
+defineProps<{
+  title?: string;
+}>();
+
+const emit = defineEmits<{
+  click: [];
+}>();
+</script>
+
+<template>
+  <DropdownMenuItem @click="emit('click')">
+    <Copy class="size-4" />
+    {{ title ?? 'Duplicate' }}
+  </DropdownMenuItem>
+</template>
