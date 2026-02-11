1ffd329fdd03055e46dcbc9e6e69ed4991404742
combizera <ycombi349@gmail.com>
2026-02-11 20:09:24 -0300

    feat(RecurringTransactionCrudTest): add tests for Transaction

---
+++ tests/Feature/RecurringTransaction/RecurringTransactionToggleTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\RecurringTransaction;
+use App\Models\User;
+
+beforeEach(function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+});
+
+// TOGGLE
+it('should be able to toggle recurring transaction active status', function () {
+    $recurring = RecurringTransaction::factory()->create([
+        'user_id'   => auth()->id(),
+        'is_active' => true,
+    ]);
+
+    $response = $this->patch(route('recurring.toggle', $recurring->id));
+
+    $response->assertRedirect(route('recurring.index'));
+
+    $this->assertDatabaseHas('recurring_transactions', [
+        'id'        => $recurring->id,
+        'is_active' => false,
+    ]);
+});
+
+it('should not be able to toggle recurring transaction that you do not own', function () {
+    $recurring = RecurringTransaction::factory()->create();
+
+    $response = $this->patch(route('recurring.toggle', $recurring->id));
+
+    $response->assertStatus(403);
+});
