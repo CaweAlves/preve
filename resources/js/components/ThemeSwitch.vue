2e11102247d1b9589a2ba1540433cf96b92853b5
combizera <ycombi349@gmail.com>
2026-02-25 20:25:48 -0300

    feat(ThemeSwitch): update theme toggle switch

--- resources/js/components/ThemeSwitch.vue
+++ resources/js/components/ThemeSwitch.vue
@@ -15,7 +15,7 @@ const toggle = (checked: boolean) => {
   <div>
     <div class="relative inline-grid h-7 grid-cols-[1fr_1fr] items-center text-sm font-medium">
       <SwitchRoot
-        :checked="appearance === 'dark'"
+        :modelValue="appearance === 'dark'"
         class="data-[state=checked]:bg-input/50 data-[state=unchecked]:bg-input/50 peer absolute inset-0 h-[inherit] w-auto cursor-pointer rounded-full [&_span]:h-full [&_span]:w-1/2 [&_span]:transition-transform [&_span]:duration-300 [&_span]:[transition-timing-function:cubic-bezier(0.16,1,0.3,1)] data-[state=checked]:[&_span]:translate-x-full rtl:data-[state=checked]:[&_span]:-translate-x-full"
         @update:modelValue="toggle"
       >
@@ -24,12 +24,12 @@ const toggle = (checked: boolean) => {
       <span
         class="peer-data-[state=checked]:text-muted-foreground/70 pointer-events-none relative ms-0.5 flex min-w-8 items-center justify-center text-center"
       >
-        <Moon :size="16" :stroke-width="2" aria-hidden="true" />
+        <Sun :size="16" :stroke-width="2" aria-hidden="true" />
       </span>
       <span
         class="peer-data-[state=unchecked]:text-muted-foreground/70 pointer-events-none relative me-0.5 flex min-w-8 items-center justify-center text-center"
       >
-        <Sun :size="16" :stroke-width="2" aria-hidden="true" />
+        <Moon :size="16" :stroke-width="2" aria-hidden="true" />
       </span>
     </div>
     <span class="sr-only">Toggle theme</span>
